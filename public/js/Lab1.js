fe446bdcec9548e649dd812141e49573a374b9b9
vantn <vantn@viettelsoftware.net>
2023-04-24 05:21:10 +0700

    lab 1

--- public/js/Lab1.js
+++ public/js/Lab1.js
@@ -1,45 +1,50 @@
 window.onload = function (){
-    /* Khai bao bien */
-    var seconds = 0;
-    var tens = 0; // 100 = 1000 milisecconds
-    var appendSeconds = document.getElementById("seconds")
-    var appendTens = document.getElementById("tens")
-    var startButton = document.getElementById("button-start")
-    var stopButton = document.getElementById("button-stop")
-    var resetButton = document.getElementById("button-reset")
-    var interval;
-    startButton.onclick = function (){
-        clearInterval(interval);
-        interval = setInterval(timer,10)
-    }
-    stopButton.onclick = () => { clearInterval(interval); }
-    resetButton.onclick = function (){
-        clearInterval(interval);
-        tens = 0;
-        seconds = 0;
-        appendTens.innerHTML = "00";
-        appendSeconds.innerHTML = "00";
-    }
-    function timer(){
-        tens ++
-        if(tens <= 9){
-            appendTens.innerHTML = "0"+tens
-        }
-        if(tens > 9){
-            appendTens.innerHTML = tens
-        }
-        if( tens > 99){
-            seconds++;
-            appendSeconds.innerHTML = "0" + seconds;
-            tens = 0;
-            appendTens.innerHTML = "0" + tens;
-        }
-        if(seconds <= 9){
-            appendSeconds.innerHTML = "0"+seconds;
-        }else{
-            appendSeconds.innerHTML = seconds;
-        }
+   var x = 5;
+   x = (x === 5) ? 6 : 0;
+   x = (x === 5) ? 6 : 0;
+   console.log(x);
 
-    }
+    // /* Khai bao bien */
+    // var seconds = 0;
+    // var tens = 0; // 100 = 1000 milisecconds
+    // var appendSeconds = document.getElementById("seconds")
+    // var appendTens = document.getElementById("tens")
+    // var startButton = document.getElementById("button-start")
+    // var stopButton = document.getElementById("button-stop")
+    // var resetButton = document.getElementById("button-reset")
+    // var interval;
+    // startButton.onclick = function (){
+    //     clearInterval(interval);
+    //     interval = setInterval(timer,10)
+    // }
+    // stopButton.onclick = () => { clearInterval(interval); }
+    // resetButton.onclick = function (){
+    //     clearInterval(interval);
+    //     tens = 0;
+    //     seconds = 0;
+    //     appendTens.innerHTML = "00";
+    //     appendSeconds.innerHTML = "00";
+    // }
+    // function timer(){
+    //     tens ++
+    //     if(tens <= 9){
+    //         appendTens.innerHTML = "0"+tens
+    //     }
+    //     if(tens > 9){
+    //         appendTens.innerHTML = tens
+    //     }
+    //     if( tens > 99){
+    //         seconds++;
+    //         appendSeconds.innerHTML = "0" + seconds;
+    //         tens = 0;
+    //         appendTens.innerHTML = "0" + tens;
+    //     }
+    //     if(seconds <= 9){
+    //         appendSeconds.innerHTML = "0"+seconds;
+    //     }else{
+    //         appendSeconds.innerHTML = seconds;
+    //     }
+    //
+    // }
 
 }
